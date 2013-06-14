cdb6d49cc945d12c1eadaf081541b81f2379d096
alee chou <zq@alee2002.com>
2013-06-14 21:42:51 +0800

    + AOP for Function

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 global._ = require("./public/lib/3party/underscore-1.4.4.js") ;
 require("./lib/system/reset.js") ;
 require("./lib/mvc/Former.js") ;
+require("./lib/AOP.js") ;
 
 module.exports = require("./lib/system/Platform.js") ;
 
